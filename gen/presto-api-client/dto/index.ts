99939e43cacf0a4216ab13229ba93d962a51c48a
koga456 <yutakoga1002@gmail.com>
2022-06-16 21:21:55 +0900

    Add generated api and dto.

---
+++ gen/presto-api-client/dto/index.ts
@@ -0,0 +1,7 @@
+export * from './error-response';
+export * from './position';
+export * from './register-position';
+export * from './register-user-request';
+export * from './user';
+export * from './user-detail';
+export * from './user-list-response';
